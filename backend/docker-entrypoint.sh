543e832474dadf0a79eb602aadc391adb84a2229
NexusTech Admin <admin@nexustech.com>
2026-06-07 12:36:29 +0200

    Configure docker entrypoint for production migrations

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -3,5 +3,9 @@
 # Substitute default port 80 with Cloud Run's dynamic PORT environment variable (default to 8080 if not set)
 sed -i "s/80/${PORT:-8080}/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
 
+# Run migrations
+echo "Running migrations..."
+php artisan migrate --force
+
 # Start Apache in the foreground
 exec apache2-foreground
